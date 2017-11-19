c2c745564cf4db5a7ce4156000fef5bd83cc6bf4
Cy <chernyee@msn.com>
2017-11-20 00:36:31 +0800

    Implemented manual/setting mode, code refactoring

--- code/code.ino
+++ code/code.ino
@@ -6,120 +6,208 @@
   Change History
   --------------
   2017-11-19  Cy  Created file
+  2017-11-19  Cy  Implemented config mode & code refactoring
  **/
 #include <Servo.h>
  
-Servo esc1;           // control motor 1
-Servo esc2;           // control motor 2
-int value = 1000;     // default arming mode
-int prevState = 0;    // default external event (pin state)
-char c;
+#define SIGNAL_PIN    7
+#define CONFIG_PIN    8
+#define COUNT         2
+#define MIN_US        1000
+#define MAX_US        2000
 
-void ShowMenu()
+typedef enum {
+  NORMAL,
+  MANUAL
+} mode_t;
+  
+typedef struct { 
+  uint8_t pinNumber;
+  int throttle_us;
+  Servo servo; 
+} esc_t;
+
+esc_t list[COUNT] = { 
+  { 9 , MIN_US, Servo() },  // motor 1 -> pin 9
+  { 10, MIN_US, Servo() }   // motor 2 -> pin 10
+};
+
+mode_t mode = NORMAL;
+int selected = 0;
+int state = LOW;
+
+void showMenu()
 {
-  Serial.println("");
-  Serial.println("Menu - Version 1.2-2017-1117");
-  Serial.println("----------------------------------");
-  Serial.println("1> Turn Off (0ms)");
-  Serial.println("2> Stop / Arming (1000ms)");
-  Serial.println("3> Step Up (+50ms)");
-  Serial.println("4> Step Downn (-50ms)");
-  Serial.println("5> Max Out / Config. Mode (2000ms)");
-  Serial.println("6> Show this menu");
-  Serial.println("");
-  Serial.print("Current Value: ");
-  Serial.println(value);
+  Serial.println();
+  Serial.println(F("Menu - Version 2.0 (2017-11-19)"));
+  Serial.println(F("-------------------------------"));
+  Serial.println(F("1. Min. Throttle - 10ms"));
+  Serial.println(F("2. Max. Throttle - 20ms"));
+  Serial.println(F("3. Step Up"));
+  Serial.println(F("4. Step Down"));
+  Serial.println(F("5. Turn Off Pulse Signal"));
+  Serial.println(F("6. Status"));
+  if (mode == MANUAL) {
+      Serial.print(F("7. Select Motor - "));
+      Serial.print(selected + 1);
+      Serial.print(F(" of "));
+      Serial.println(COUNT);
+  }
+  Serial.println(F("0. Show Menu"));
+  Serial.println();
 }
 
-void setValue(int val) 
+void wait(int times, int ms)
 {
-  esc1.writeMicroseconds(val);
-  esc2.writeMicroseconds(val);
+  for (uint8_t i = 0; i < times; i++) {
+    digitalWrite(LED_BUILTIN, (i & 1 ? LOW : HIGH));
+    delay(ms);
+  }  
+}
+
+void setValue(int value, esc_t &esc)
+{
+  esc.servo.writeMicroseconds(esc.throttle_us = value);
+}
+
+void setThrottle(int val) 
+{
+  for (uint8_t i = 0; i < COUNT; i++) {
+    setValue(val, list[i]);
+  }
+}
+
+int calcThrottle(int throttle)
+{
+  return throttle > MAX_US ? MAX_US : (throttle < MIN_US ? MIN_US : throttle);
+}
+
+void processCommand(char cmd)
+{
+  switch (cmd)
+  {
+    case '0':
+      showMenu();
+      break;
+
+    case '1':
+      if (mode == MANUAL) {
+        setValue(MIN_US, list[selected]);
+      } else {
+        setThrottle(MIN_US);
+      }
+      break;
+
+    case '2':
+      if (mode == MANUAL) {
+        setValue(MAX_US, list[selected]);
+      } else {
+        setThrottle(MAX_US);
+      }
+      break;
+
+    case '3':
+      if (mode == MANUAL) {
+        setValue(calcThrottle(list[selected].throttle_us + 50), list[selected]);
+      } else {
+        setThrottle(calcThrottle(list[0].throttle_us + 50));
+      }
+      break;
+      
+    case '4':
+      if (mode == MANUAL) {
+        setValue(calcThrottle(list[selected].throttle_us - 50), list[selected]);
+      } else {
+        setThrottle(calcThrottle(list[0].throttle_us - 50));
+      }
+      break;
+
+    case '5':
+      if (mode == MANUAL) {
+        setValue(0, list[selected]);
+      } else {
+        setThrottle(0);
+      }
+      break;
+      
+    case '6':
+      Serial.print(F("Mode         : "));
+      Serial.println(mode == NORMAL ? F("Normal") : F("Manual"));
+      Serial.print(F("Throttle(ms) : "));
+      Serial.println(list[mode == NORMAL ? 0 : selected].throttle_us);    
+      Serial.println();
+      break;
+      
+    case '7':
+      if (mode == MANUAL) {
+        selected = ++selected % COUNT;
+        Serial.print(F("Selected motor "));
+        Serial.print(selected + 1);
+        Serial.print(F(" of "));
+        Serial.print(COUNT);
+        Serial.println(F("!"));
+      } else {
+        Serial.println(F("Not support in current mode!"));
+        Serial.println();
+      }
+      break;
+      
+    default:
+      Serial.print(F("Invalid command: "));
+      Serial.println(cmd);
+  }
 }
 
 void setup()
 {
-  Serial.begin(115200);           // start serial at 115200 baud
-  pinMode(LED_BUILTIN, OUTPUT);   // use for external event indicator
-  pinMode(7, INPUT_PULLUP);       // configure pin 7 as input with internal pullup motor from external (on / off motors)
-  esc1.attach( 9);                // attach Servo object to pin 9 
-  esc2.attach(10);                // attach Servo object to pin 10
-  setValue(value);                // arming motor 1 & 2
-  
-  // wait for 5 seconds
-  for (int i = 0; i < 10; i++) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(250);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(250);
+  // configure serial interface at 115200 baud
+  Serial.begin(115200);
+
+  // configure input/output pins
+  pinMode(LED_BUILTIN, OUTPUT);         // event indicator LED
+  pinMode(SIGNAL_PIN, INPUT_PULLUP);    // input (pullup mode) from external to turn on/off the motors
+  pinMode(CONFIG_PIN, INPUT_PULLUP);    // input (pullup mode) to enter config mode
+
+  // read config pin
+  if (!digitalRead(CONFIG_PIN)) {
+    mode = MANUAL;
   }
   
-  ShowMenu();
+  // configure pins to the Servo objects
+  for (int i = 0; i < COUNT; i++) {
+    if (mode == MANUAL) {
+      list[i].throttle_us = 0;
+    }
+    list[i].servo.attach(list[i].pinNumber);
+    list[i].servo.writeMicroseconds(list[i].throttle_us);
+  }
+
+  // for normal mode, wait for 5 sec and while waiting, blink the led
+  if (mode == NORMAL) {
+    wait(20, 250);  
+  }
+  
+  // show menu
+  showMenu();
 }
- 
+
+void processSignal(int value) 
+{
+  if (state != value) {
+    if (value == LOW) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      setThrottle(MAX_US);
+    } else {
+      digitalWrite(LED_BUILTIN, LOW);
+      setThrottle(MIN_US);
+    }
+    state = value;
+  }
+}
+
 void loop()
 {
-  int currState;
-  
-  setValue(value);
-
-  if ((currState = digitalRead(7)) != prevState)
-  {
-    if (!currState)
-    {
-      digitalWrite(LED_BUILTIN, HIGH);
-      value = 2000;
-    }
-    else
-    {
-      digitalWrite(LED_BUILTIN, LOW);
-      value = 1000;
-    }
-    prevState = currState;
-  }
-
-  if (Serial.available()) 
-  {
-    c = Serial.read();
-    
-    switch (c)
-    {
-      case '1':
-        
-        value = 0; 
-        break;
-        
-      case '2':
-      
-        value = 1000; 
-        break;
-      
-      case '3': // step up by 50ms
-
-        value = ((value + 50) <= 2000) ? value + 50 : 2000; 
-        break;
-      
-      case '4': // step down by 50ms
-      
-        value = ((value - 50) >= 1000) ? value - 50 : 1000; 
-        break;
-      
-      case '5':
-        
-        value = 2000; 
-        break;
-
-      case '6':
-      
-        ShowMenu();
-        break;
-      
-      default:
-
-        Serial.print("Invalid options: ");
-        Serial.println(c, HEX);
-        break;
-    }
-  }
+  if (mode == NORMAL) processSignal(digitalRead(SIGNAL_PIN));
+  if (Serial.available()) processCommand(Serial.read());
 }
 
